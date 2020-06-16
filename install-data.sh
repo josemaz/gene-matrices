9b3280c9558d8d8eb94ea42c633677ed17fa9c76
JoseZ <josema.genomics@gmail.com>
2020-06-16 14:41:43 -0500

    Create: install data

---
+++ install-data.sh
@@ -0,0 +1,10 @@
+# split -b 30m gene-expression.tgz data.
+
+cat data.* > gene-expression.tgz
+tar xzvf gene-expression.tgz
+cd Data/Expression/
+md5um -c checksums.md5
+echo "DATA INSTALLED SUCCESSFULLY"
+
+# clean
+# rm -rf Data gene-expression.tgz
