6a413b1bb6d86167ddc5b991a54175a86c4163e4
Jose Maria Zamora <josema.genomics@gmail.com>
2020-09-08 13:06:22 -0500

    Update: Download Data

--- download-data.sh
+++ download-data.sh
@@ -1,7 +1,8 @@
 
+echo "Downloading in Data/Expression"
 mkdir -p Data/Expression
 cd Data/Expression
-url="http://espinal3.inmegen.gob.mx:5270/genecorr/clean/"
+url="http://espinal3.inmegen.gob.mx:5270/genecorr/subtipos-originales/"
 wget -o down.log --cut-dirs=2 -r -nH -A '*-All.txt' $url
-[[ "$?" -ne 0 ]] && echo "Download error: check Data/down.log" && exit 15
+[[ "$?" -ne 0 ]] && echo "Download error: check down.log" && exit 15
 echo "Succesfully download"
