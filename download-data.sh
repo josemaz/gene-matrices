7475093ac850834fc3faa8aab4b14833dc1fb162
Jose Maria Zamora <josema.genomics@gmail.com>
2020-09-08 13:02:09 -0500

    Update: Download Data

--- download-data.sh
+++ download-data.sh
@@ -2,6 +2,6 @@
 mkdir -p Data/Expression
 cd Data/Expression
 url="http://espinal3.inmegen.gob.mx:5270/genecorr/clean/"
-wget -o down.log --cut-dirs=2 -r -nH -A '*.tsv' $url
+wget -o down.log --cut-dirs=2 -r -nH -A '*-All.txt' $url
 [[ "$?" -ne 0 ]] && echo "Download error: check Data/down.log" && exit 15
 echo "Succesfully download"
