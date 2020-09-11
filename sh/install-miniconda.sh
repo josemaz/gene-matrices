982d235703321bf89e67a6c304af82de90ea05c0
josemaz <47672759+josemaz@users.noreply.github.com>
2020-09-11 17:05:05 -0500

    Update install-miniconda.sh

--- sh/install-miniconda.sh
+++ sh/install-miniconda.sh
@@ -2,9 +2,11 @@
 
 condadir="${HOME}/bioconda"
 cd /tmp
-wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-bash Miniconda3-latest-Linux-x86_64.sh -p $condadir -b -s
-rm -rf /tmp/Miniconda3-latest-Linux-x86_64.sh
+pkg="Miniconda3-latest-Linux-x86_64.sh"
+pkg="Miniconda3-py37_4.8.3-Linux-x86_64.sh"
+wget "https://repo.anaconda.com/miniconda/${pkg}"
+bash ${pkg} -p $condadir -b -s
+rm -rf /tmp/${pkg}
 . $condadir/bin/activate
 conda install -y numpy pandas seaborn palettable termcolor
 easy_install trash-cli
