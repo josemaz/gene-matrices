990d85459ec92c600aa54cc6ca04daca21bd394d
Jose Maria Zamora <josema.genomics@gmail.com>
2020-09-11 13:17:22 -0500

    Update: Organize shell scripts, Add colors and timers

---
+++ sh/02-clustering.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+srcpath=$(dirname $(realpath $0))
+. $srcpath/bash_colors.sh
+
+
+dirclean="Data/clean"
+
+t1=$SECONDS
+# for t in Healthy Basal Her2 LumA LumB
+for t in Her2
+do 
+	clr_bold clr_green "Using ${t} subtype"
+	
+	clr_bold clr_white "Cluster ALL genes"
+	t2=$SECONDS
+	julia julia/02-clusters.jl -c ${dirclean}/${t}-clean.tsv
+	clr_bold clr_blue "[Cluster ALL genes] $(( ($SECONDS-t2) / 60)) min. elapsed"
+	
+	clr_bold clr_white "Cluster by chrom"
+	t3=$SECONDS
+	julia julia/02-clusters-chrom.jl ${dirclean}/${t}-clean.tsv
+	clr_bold clr_blue "[Cluster by chrom] $(( ($SECONDS-t3) / 60)) min. elapsed"
+done
+clr_bold clr_blue "[Total time] $(( ($t1-a) / 60)) min. elapsed"
+clr_bold clr_green "Successfully finished"
--- sh/03-Plot.sh
+++ sh/03-Plot.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+srcpath=$(dirname $(realpath $0))
+. $srcpath/bash_colors.sh
+
 dirclean="Data/clean"
 
 for t in Healthy Basal Her2 LumA LumB
 do 
-	echo "Using ${t} subtype"
+	clr_bold clr_green "Using ${t} subtype"
 	python py/03-plot-pearson.py Data/clean/${t}-clean.tsv
 done
